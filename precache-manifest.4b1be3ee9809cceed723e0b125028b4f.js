aad1b19294c7c508d112c8ce1530ef455c98884b
Vaibhav Kushwaha <vaibhavkushwaha11@gmail.com>
2022-01-21 00:18:32 +0530

    Updates

---
+++ precache-manifest.4b1be3ee9809cceed723e0b125028b4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e5b127dd921823d80316d95282f3955",
+    "url": "/placeslog-react-frontend/index.html"
+  },
+  {
+    "revision": "d8f5642ca2bdd3f5fc43",
+    "url": "/placeslog-react-frontend/static/css/main.e9c48add.chunk.css"
+  },
+  {
+    "revision": "10de50ed0a6788087632",
+    "url": "/placeslog-react-frontend/static/js/2.7c57f51f.chunk.js"
+  },
+  {
+    "revision": "d8f5642ca2bdd3f5fc43",
+    "url": "/placeslog-react-frontend/static/js/main.6690dfe6.chunk.js"
+  },
+  {
+    "revision": "324b9edb17f1e8f822a3",
+    "url": "/placeslog-react-frontend/static/js/runtime-main.905544a8.js"
+  }
+]);
